68ab89499daca3894a72805aeaef2c3ca1d0b0a0
iamdanfox <iamdanfox@gmail.com>
2018-05-22 17:35:35 +0100

    Add version

--- scripts/circle-publish-dist.sh
+++ scripts/circle-publish-dist.sh
@@ -8,7 +8,7 @@ fi
 artifact_version=$CIRCLE_TAG
 artifact_file="conjure-typescript-$artifact_version.tgz"
 
-upload_url="https://api.bintray.com/content/palantir/releases/conjure-typescript/com/palantir/conjure/typescript/conjure-typescript/${artifact_version}/${artifact_file}"
+upload_url="https://api.bintray.com/content/palantir/releases/conjure-typescript/${artifact_version}/com/palantir/conjure/typescript/conjure-typescript/${artifact_version}/${artifact_file}"
 echo "publishing $1 to $upload_url"
 set +x
 curl --fail -v -X PUT "$upload_url?publish=1" -T "$1" -u $BINTRAY_USERNAME:$BINTRAY_PASSWORD
