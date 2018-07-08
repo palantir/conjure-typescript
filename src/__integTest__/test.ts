57e97da72c069542f8550a6ae8bac06328196401
forozco <forozco@palantir.com>
2018-07-08 20:43:24 +0100

    pass most tests

--- src/__integTest__/test.ts
+++ src/__integTest__/test.ts
@@ -16,45 +16,54 @@
  */
 
 import { assert } from "chai";
-import { DefaultHttpApiBridge } from "conjure-client";
-import { AutoDeserializeService, ITestCasesYml } from "./__generated__";
+import { DefaultHttpApiBridge, isConjureError } from "conjure-client";
+import { AutoDeserializeService, ITestCases } from "./__generated__";
+import { AutoDeserializeConfirmService } from "./__generated__/conjure-compliance";
 // HACKHACK to load test-cases
 // tslint:disable:no-var-requires
-const testCasesFile: ITestCasesYml = require("../../resources/test-cases.json");
+const testCasesFile: ITestCases = require("../../resources/test-cases.json");
 
 describe("Array", () => {
-    const testService = new AutoDeserializeService(
-        new DefaultHttpApiBridge({
-            baseUrl: "http://localhost:8000",
-            userAgent: {
-                productName: "conjure-typescript",
-                productVersion: "0.0.0",
-            },
-        }),
-    );
+    const bridge = new DefaultHttpApiBridge({
+        baseUrl: "http://localhost:8000",
+        userAgent: {
+            productName: "conjure-typescript",
+            productVersion: "0.0.0",
+        },
+        fetch: (url: string | Request, init?: RequestInit) => {
+            if (init && init.headers) {
+                delete (init.headers as { [x: string]: string })["Fetch-User-Agent"];
+            }
+            return window.fetch(url, init);
+        },
+    });
+    const testService = new AutoDeserializeService(bridge);
+    const confirmService = new AutoDeserializeConfirmService(bridge);
 
-    Object.keys(testCasesFile.autoDeserialize).map(endpointName => {
-        const testCases = testCasesFile.autoDeserialize[endpointName];
+    Object.keys(testCasesFile.client.autoDeserialize).map(endpointName => {
+        const testCases = testCasesFile.client.autoDeserialize[endpointName];
         testCases.positive.map((_, i) => {
-            it(`${endpointName}_${i}_pass`, automaticTest(testService, endpointName, i, true));
+            it(`${endpointName}_${i}_pass`, automaticTest(endpointName, i, true));
         });
         testCases.negative.map((_, i) => {
             const index = i + testCases.positive.length;
-            it(`${endpointName}_${index}_fail`, automaticTest(testService, endpointName, index, false));
+            it(`${endpointName}_${index}_fail`, automaticTest(endpointName, index, false));
         });
     });
-});
 
-function automaticTest(testService: AutoDeserializeService, endpointName: string, index: number, shouldPass: boolean) {
-    return async () => {
-        let response: any = null;
-        try {
-            response = await (testService as any)[endpointName](index);
-        } finally {
-            assert.equal((!shouldPass && response == null) || (shouldPass && response != null), true);
-            if (shouldPass) {
-                await testService.confirm(endpointName, index, response);
+    function automaticTest(endpointName: string, index: number, shouldPass: boolean) {
+        return async () => {
+            let response: any = null;
+            try {
+                response = await (testService as any)[endpointName](index);
+            } catch (e) {
+                if (!isConjureError(e) || shouldPass) {
+                    assert.fail();
+                }
             }
-        }
-    };
-}
+            if (shouldPass) {
+                await confirmService.confirm(endpointName, index, response);
+            }
+        };
+    }
+});
