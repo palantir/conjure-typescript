91f11c9929bd4d8e7261899cb89da0ebeb5155ff
Benjamin Duffield <bduffield@palantir.com>
2019-07-23 23:27:45 -0400

    generate for failing test + manually edit to match desired output

---
+++ src/commands/generate/__tests__/resources/services/outOfOrderPathService.ts
@@ -0,0 +1,30 @@
+import { IHttpApiBridge, MediaType } from "conjure-client";
+
+export interface IOutOfOrderPathService {
+    foo(param1: string, param2: string): Promise<void>;
+}
+
+export class OutOfOrderPathService {
+    constructor(private bridge: IHttpApiBridge) {
+    }
+
+    public foo(param1: string, param2: string): Promise<void> {
+        return this.bridge.callEndpoint<void>({
+            data: undefined,
+            endpointName: "foo",
+            endpointPath: "/{param2}/{param1}",
+            headers: {
+            },
+            method: "GET",
+            pathArguments: [
+                param2,
+
+                param1,
+            ],
+            queryArguments: {
+            },
+            requestMediaType: MediaType.APPLICATION_JSON,
+            responseMediaType: MediaType.APPLICATION_JSON,
+        });
+    }
+}
