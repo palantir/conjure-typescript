c4201af6c80019634508801d767b87577bcf6bb3
Alex de la Mare <adelamare@palantir.com>
2022-07-15 15:50:59 +1000

    Add readonly collections return visitor tests

--- src/commands/generate/__tests__/tsReturnTypeVisitorTest.ts
+++ src/commands/generate/__tests__/tsReturnTypeVisitorTest.ts
@@ -18,164 +18,268 @@
 import { IType, ITypeDefinition, PrimitiveType } from "conjure-api";
 import { TsReturnTypeVisitor } from "../tsReturnTypeVisitor";
 import { createHashableTypeName } from "../utils";
-import { FLAVORED_TYPE_GENERATION_FLAGS } from "./resources/constants";
+import { DEFAULT_TYPE_GENERATION_FLAGS, FLAVORED_TYPE_GENERATION_FLAGS, READONLY_COLLECTION_TYPE_GENERATION_FLAGS } from "./resources/constants";
+
+const objectName = { name: "Object", package: "" };
+const objectReference = IType.reference(objectName);
+const object = ITypeDefinition.object({
+    fields: [],
+    typeName: objectName,
+});
+
+const aliasName = { name: "Alias", package: "" };
+const aliasReference = IType.reference(aliasName);
+const alias = ITypeDefinition.alias({
+    alias: { primitive: PrimitiveType.STRING, type: "primitive" },
+    typeName: aliasName,
+});
+
+const binaryAliasName = { name: "BinaryAlias", package: "" };
+const binaryAliasReference = IType.reference(binaryAliasName);
+const binaryAlias = ITypeDefinition.alias({
+    alias: { primitive: PrimitiveType.BINARY, type: "primitive" },
+    typeName: binaryAliasName,
+});
+
+const enumName = { name: "Enum", package: "" };
+const enumReference = IType.reference(enumName);
+const enumType = ITypeDefinition.enum_({
+    typeName: enumName,
+    values: [{ value: "FOO" }],
+});
+
+const fakeTypeName = { name: "someObject", package: "com.palantir.example" };
 
 describe("TsTypeVisitor", () => {
-    const objectName = { name: "Object", package: "" };
-    const objectReference = IType.reference(objectName);
-    const object = ITypeDefinition.object({
-        fields: [],
-        typeName: objectName,
-    });
 
-    const aliasName = { name: "Alias", package: "" };
-    const aliasReference = IType.reference(aliasName);
-    const alias = ITypeDefinition.alias({
-        alias: { primitive: PrimitiveType.STRING, type: "primitive" },
-        typeName: aliasName,
-    });
+    describe("with default generation flags", () => {
 
-    const binaryAliasName = { name: "BinaryAlias", package: "" };
-    const binaryAliasReference = IType.reference(binaryAliasName);
-    const binaryAlias = ITypeDefinition.alias({
-        alias: { primitive: PrimitiveType.BINARY, type: "primitive" },
-        typeName: binaryAliasName,
-    });
-
-    const enumName = { name: "Enum", package: "" };
-    const enumReference = IType.reference(enumName);
-    const enumType = ITypeDefinition.enum_({
-        typeName: enumName,
-        values: [{ value: "FOO" }],
-    });
-
-    const fakeTypeName = { name: "someObject", package: "com.palantir.example" };
-
-    const visitor = new TsReturnTypeVisitor(
-        new Map<string, ITypeDefinition>([
-            [createHashableTypeName(objectName), object],
-            [createHashableTypeName(aliasName), alias],
-            [createHashableTypeName(binaryAliasName), binaryAlias],
-            [createHashableTypeName(enumName), enumType],
-        ]),
-        fakeTypeName,
-        false,
-        FLAVORED_TYPE_GENERATION_FLAGS,
-    );
-
-    const topLevelVisitor = new TsReturnTypeVisitor(
-        new Map<string, ITypeDefinition>([
-            [createHashableTypeName(objectName), object],
-            [createHashableTypeName(aliasName), alias],
-            [createHashableTypeName(binaryAliasName), binaryAlias],
-            [createHashableTypeName(enumName), enumType],
-        ]),
-        fakeTypeName,
-        true,
-        FLAVORED_TYPE_GENERATION_FLAGS,
-    );
-
-    it("returns primitive types", () => {
-        expect(visitor.primitive(PrimitiveType.STRING)).toEqual("string");
-        expect(visitor.primitive(PrimitiveType.DATETIME)).toEqual("string");
-        expect(visitor.primitive(PrimitiveType.INTEGER)).toEqual("number");
-        expect(visitor.primitive(PrimitiveType.DOUBLE)).toEqual('number | "NaN"');
-        expect(visitor.primitive(PrimitiveType.SAFELONG)).toEqual("number");
-        expect(visitor.primitive(PrimitiveType.BINARY)).toEqual("string");
-        expect(visitor.primitive(PrimitiveType.ANY)).toEqual("any");
-        expect(visitor.primitive(PrimitiveType.BOOLEAN)).toEqual("boolean");
-        expect(visitor.primitive(PrimitiveType.RID)).toEqual("string");
-        expect(visitor.primitive(PrimitiveType.BEARERTOKEN)).toEqual("string");
-        expect(visitor.primitive(PrimitiveType.UUID)).toEqual("string");
-    });
-
-    it("has correct top level types", () => {
-        expect(topLevelVisitor.primitive(PrimitiveType.STRING)).toEqual("string");
-        expect(topLevelVisitor.primitive(PrimitiveType.DATETIME)).toEqual("string");
-        expect(topLevelVisitor.primitive(PrimitiveType.INTEGER)).toEqual("number");
-        expect(topLevelVisitor.primitive(PrimitiveType.DOUBLE)).toEqual('number | "NaN"');
-        expect(topLevelVisitor.primitive(PrimitiveType.SAFELONG)).toEqual("number");
-        expect(topLevelVisitor.primitive(PrimitiveType.BINARY)).toEqual("ReadableStream<Uint8Array>");
-        expect(topLevelVisitor.primitive(PrimitiveType.ANY)).toEqual("any");
-        expect(topLevelVisitor.primitive(PrimitiveType.BOOLEAN)).toEqual("boolean");
-        expect(topLevelVisitor.primitive(PrimitiveType.RID)).toEqual("string");
-        expect(topLevelVisitor.primitive(PrimitiveType.BEARERTOKEN)).toEqual("string");
-        expect(topLevelVisitor.primitive(PrimitiveType.UUID)).toEqual("string");
-    });
-
-    it("produces error for unknown reference", () => {
-        const tsType = () =>
-            new TsReturnTypeVisitor(new Map(), fakeTypeName, false, FLAVORED_TYPE_GENERATION_FLAGS).reference(objectName);
-        expect(tsType).toThrowError(/unknown reference type/);
-    });
-
-    it("returns reference type", () => {
-        expect(visitor.reference(objectName)).toEqual("IObject");
-    });
-
-    it("follows alias reference", () => {
-        expect(visitor.reference(aliasName)).toEqual("IAlias");
-        expect(visitor.reference(binaryAliasName)).toEqual("string");
-        expect(topLevelVisitor.reference(aliasName)).toEqual("IAlias");
-        expect(topLevelVisitor.reference(binaryAliasName)).toEqual("ReadableStream<Uint8Array>");
-    });
-
-    it("returns enum reference without I prefix", () => {
-        expect(visitor.reference(enumName)).toEqual("Enum");
-    });
-
-    it("returns optional type", () => {
-        expect(visitor.optional({ itemType: objectReference })).toEqual("IObject | null");
-        expect(visitor.optional({ itemType: binaryAliasReference })).toEqual("string | null");
-        expect(topLevelVisitor.optional({ itemType: binaryAliasReference })).toEqual(
-            "ReadableStream<Uint8Array> | null",
+        const visitor = new TsReturnTypeVisitor(
+            new Map<string, ITypeDefinition>([
+                [createHashableTypeName(objectName), object],
+                [createHashableTypeName(aliasName), alias],
+                [createHashableTypeName(binaryAliasName), binaryAlias],
+                [createHashableTypeName(enumName), enumType],
+            ]),
+            fakeTypeName,
+            false,
+            DEFAULT_TYPE_GENERATION_FLAGS,
         );
-    });
 
-    it("returns list type", () => {
-        expect(visitor.list({ itemType: objectReference })).toEqual("Array<IObject>");
-        expect(visitor.list({ itemType: binaryAliasReference })).toEqual("Array<string>");
-        expect(topLevelVisitor.list({ itemType: binaryAliasReference })).toEqual("Array<string>");
-    });
-
-    it("returns set type", () => {
-        expect(visitor.set({ itemType: objectReference })).toEqual("Array<IObject>");
-        expect(visitor.set({ itemType: binaryAliasReference })).toEqual("Array<string>");
-        expect(topLevelVisitor.set({ itemType: binaryAliasReference })).toEqual("Array<string>");
-    });
-
-    it("returns map type", () => {
-        expect(visitor.map({ keyType: aliasReference, valueType: objectReference })).toEqual(
-            "{ [key: IAlias]: IObject }",
+        const topLevelVisitor = new TsReturnTypeVisitor(
+            new Map<string, ITypeDefinition>([
+                [createHashableTypeName(objectName), object],
+                [createHashableTypeName(aliasName), alias],
+                [createHashableTypeName(binaryAliasName), binaryAlias],
+                [createHashableTypeName(enumName), enumType],
+            ]),
+            fakeTypeName,
+            true,
+            DEFAULT_TYPE_GENERATION_FLAGS,
         );
-        expect(visitor.map({ keyType: aliasReference, valueType: binaryAliasReference })).toEqual(
-            "{ [key: IAlias]: string }",
+
+        it("returns primitive types", () => {
+            expect(visitor.primitive(PrimitiveType.STRING)).toEqual("string");
+            expect(visitor.primitive(PrimitiveType.DATETIME)).toEqual("string");
+            expect(visitor.primitive(PrimitiveType.INTEGER)).toEqual("number");
+            expect(visitor.primitive(PrimitiveType.DOUBLE)).toEqual('number | "NaN"');
+            expect(visitor.primitive(PrimitiveType.SAFELONG)).toEqual("number");
+            expect(visitor.primitive(PrimitiveType.BINARY)).toEqual("string");
+            expect(visitor.primitive(PrimitiveType.ANY)).toEqual("any");
+            expect(visitor.primitive(PrimitiveType.BOOLEAN)).toEqual("boolean");
+            expect(visitor.primitive(PrimitiveType.RID)).toEqual("string");
+            expect(visitor.primitive(PrimitiveType.BEARERTOKEN)).toEqual("string");
+            expect(visitor.primitive(PrimitiveType.UUID)).toEqual("string");
+        });
+
+        it("has correct top level types", () => {
+            expect(topLevelVisitor.primitive(PrimitiveType.STRING)).toEqual("string");
+            expect(topLevelVisitor.primitive(PrimitiveType.DATETIME)).toEqual("string");
+            expect(topLevelVisitor.primitive(PrimitiveType.INTEGER)).toEqual("number");
+            expect(topLevelVisitor.primitive(PrimitiveType.DOUBLE)).toEqual('number | "NaN"');
+            expect(topLevelVisitor.primitive(PrimitiveType.SAFELONG)).toEqual("number");
+            expect(topLevelVisitor.primitive(PrimitiveType.BINARY)).toEqual("ReadableStream<Uint8Array>");
+            expect(topLevelVisitor.primitive(PrimitiveType.ANY)).toEqual("any");
+            expect(topLevelVisitor.primitive(PrimitiveType.BOOLEAN)).toEqual("boolean");
+            expect(topLevelVisitor.primitive(PrimitiveType.RID)).toEqual("string");
+            expect(topLevelVisitor.primitive(PrimitiveType.BEARERTOKEN)).toEqual("string");
+            expect(topLevelVisitor.primitive(PrimitiveType.UUID)).toEqual("string");
+        });
+
+        it("produces error for unknown reference", () => {
+            const tsType = () =>
+                new TsReturnTypeVisitor(new Map(), fakeTypeName, false, FLAVORED_TYPE_GENERATION_FLAGS).reference(objectName);
+            expect(tsType).toThrowError(/unknown reference type/);
+        });
+
+        it("returns reference type", () => {
+            expect(visitor.reference(objectName)).toEqual("IObject");
+        });
+
+        it("follows alias reference", () => {
+            expect(visitor.reference(aliasName)).toEqual("string");
+            expect(visitor.reference(binaryAliasName)).toEqual("string");
+            expect(topLevelVisitor.reference(aliasName)).toEqual("string");
+            expect(topLevelVisitor.reference(binaryAliasName)).toEqual("ReadableStream<Uint8Array>");
+        });
+
+        it("returns enum reference without I prefix", () => {
+            expect(visitor.reference(enumName)).toEqual("Enum");
+        });
+
+        it("returns optional type", () => {
+            expect(visitor.optional({ itemType: objectReference })).toEqual("IObject | null");
+            expect(visitor.optional({ itemType: binaryAliasReference })).toEqual("string | null");
+            expect(topLevelVisitor.optional({ itemType: binaryAliasReference })).toEqual(
+                "ReadableStream<Uint8Array> | null",
+            );
+        });
+
+        it("returns list type", () => {
+            expect(visitor.list({ itemType: objectReference })).toEqual("Array<IObject>");
+            expect(visitor.list({ itemType: binaryAliasReference })).toEqual("Array<string>");
+            expect(topLevelVisitor.list({ itemType: binaryAliasReference })).toEqual("Array<string>");
+        });
+
+        it("returns set type", () => {
+            expect(visitor.set({ itemType: objectReference })).toEqual("Array<IObject>");
+            expect(visitor.set({ itemType: binaryAliasReference })).toEqual("Array<string>");
+            expect(topLevelVisitor.set({ itemType: binaryAliasReference })).toEqual("Array<string>");
+        });
+
+        it("returns map type", () => {
+            expect(visitor.map({ keyType: aliasReference, valueType: objectReference })).toEqual(
+                "{ [key: string]: IObject }",
+            );
+            expect(visitor.map({ keyType: aliasReference, valueType: binaryAliasReference })).toEqual(
+                "{ [key: string]: string }",
+            );
+            expect(topLevelVisitor.map({ keyType: aliasReference, valueType: binaryAliasReference })).toEqual(
+                "{ [key: string]: string }",
+            );
+        });
+
+        it("returns map type with enum keys", () => {
+            const tsType = visitor.map({ keyType: enumReference, valueType: objectReference });
+            expect(tsType).toEqual(`{ [key in ${enumName.name}]?: IObject }`);
+        });
+
+        it("follows primitive external fallback", () => {
+            const unusedTypeName = { name: "Unused", package: "" };
+            const externalType = {
+                externalReference: unusedTypeName,
+                fallback: IType.primitive(PrimitiveType.ANY),
+            };
+            expect(visitor.external(externalType)).toEqual("any");
+        });
+
+        it("follows complex external fallback", () => {
+            const unusedTypeName = { name: "Unused", package: "" };
+            const externalType = {
+                externalReference: unusedTypeName,
+                fallback: IType.list({ itemType: objectReference }),
+            };
+            expect(visitor.external(externalType)).toEqual("Array<IObject>");
+        });
+    });
+
+
+    describe("with flavored generation flags", () => {
+
+        const visitor = new TsReturnTypeVisitor(
+            new Map<string, ITypeDefinition>([
+                [createHashableTypeName(objectName), object],
+                [createHashableTypeName(aliasName), alias],
+                [createHashableTypeName(binaryAliasName), binaryAlias],
+                [createHashableTypeName(enumName), enumType],
+            ]),
+            fakeTypeName,
+            false,
+            FLAVORED_TYPE_GENERATION_FLAGS,
         );
-        expect(topLevelVisitor.map({ keyType: aliasReference, valueType: binaryAliasReference })).toEqual(
-            "{ [key: IAlias]: string }",
+
+        const topLevelVisitor = new TsReturnTypeVisitor(
+            new Map<string, ITypeDefinition>([
+                [createHashableTypeName(objectName), object],
+                [createHashableTypeName(aliasName), alias],
+                [createHashableTypeName(binaryAliasName), binaryAlias],
+                [createHashableTypeName(enumName), enumType],
+            ]),
+            fakeTypeName,
+            true,
+            FLAVORED_TYPE_GENERATION_FLAGS,
         );
+
+        it("follows alias reference", () => {
+            expect(visitor.reference(aliasName)).toEqual("IAlias");
+            expect(visitor.reference(binaryAliasName)).toEqual("string");
+            expect(topLevelVisitor.reference(aliasName)).toEqual("IAlias");
+            expect(topLevelVisitor.reference(binaryAliasName)).toEqual("ReadableStream<Uint8Array>");
+        });
     });
 
-    it("returns map type with enum keys", () => {
-        const tsType = visitor.map({ keyType: enumReference, valueType: objectReference });
-        expect(tsType).toEqual(`{ [key in ${enumName.name}]?: IObject }`);
-    });
+    describe("with readonlyCollections flag", () => {
 
-    it("follows primitive external fallback", () => {
-        const unusedTypeName = { name: "Unused", package: "" };
-        const externalType = {
-            externalReference: unusedTypeName,
-            fallback: IType.primitive(PrimitiveType.ANY),
-        };
-        expect(visitor.external(externalType)).toEqual("any");
-    });
+        const visitor = new TsReturnTypeVisitor(
+            new Map<string, ITypeDefinition>([
+                [createHashableTypeName(objectName), object],
+                [createHashableTypeName(aliasName), alias],
+                [createHashableTypeName(binaryAliasName), binaryAlias],
+                [createHashableTypeName(enumName), enumType],
+            ]),
+            fakeTypeName,
+            false,
+            READONLY_COLLECTION_TYPE_GENERATION_FLAGS,
+        );
 
-    it("follows complex external fallback", () => {
-        const unusedTypeName = { name: "Unused", package: "" };
-        const externalType = {
-            externalReference: unusedTypeName,
-            fallback: IType.list({ itemType: objectReference }),
-        };
-        expect(visitor.external(externalType)).toEqual("Array<IObject>");
+        const topLevelVisitor = new TsReturnTypeVisitor(
+            new Map<string, ITypeDefinition>([
+                [createHashableTypeName(objectName), object],
+                [createHashableTypeName(aliasName), alias],
+                [createHashableTypeName(binaryAliasName), binaryAlias],
+                [createHashableTypeName(enumName), enumType],
+            ]),
+            fakeTypeName,
+            true,
+            READONLY_COLLECTION_TYPE_GENERATION_FLAGS,
+        );
+
+        it("returns list type", () => {
+            expect(visitor.list({ itemType: objectReference })).toEqual("ReadonlyArray<IObject>");
+            expect(visitor.list({ itemType: binaryAliasReference })).toEqual("ReadonlyArray<string>");
+            expect(topLevelVisitor.list({ itemType: binaryAliasReference })).toEqual("ReadonlyArray<string>");
+        });
+
+        it("returns set type", () => {
+            expect(visitor.set({ itemType: objectReference })).toEqual("ReadonlyArray<IObject>");
+            expect(visitor.set({ itemType: binaryAliasReference })).toEqual("ReadonlyArray<string>");
+            expect(topLevelVisitor.set({ itemType: binaryAliasReference })).toEqual("ReadonlyArray<string>");
+        });
+
+        it("returns map type", () => {
+            expect(visitor.map({ keyType: aliasReference, valueType: objectReference })).toEqual(
+                "{ readonly [key: string]: IObject }",
+            );
+            expect(visitor.map({ keyType: aliasReference, valueType: binaryAliasReference })).toEqual(
+                "{ readonly [key: string]: string }",
+            );
+            expect(topLevelVisitor.map({ keyType: aliasReference, valueType: binaryAliasReference })).toEqual(
+                "{ readonly [key: string]: string }",
+            );
+        });
+
+        it("returns map type with enum keys", () => {
+            const tsType = visitor.map({ keyType: enumReference, valueType: objectReference });
+            expect(tsType).toEqual(`{ readonly [key in ${enumName.name}]?: IObject }`);
+        });
+
+        it("follows complex external fallback", () => {
+            const unusedTypeName = { name: "Unused", package: "" };
+            const externalType = {
+                externalReference: unusedTypeName,
+                fallback: IType.list({ itemType: objectReference }),
+            };
+            expect(visitor.external(externalType)).toEqual("ReadonlyArray<IObject>");
+        });
     });
 });
