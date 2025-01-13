a4318dbba223cde9df4b52f1717ec66620d3c2bd
Bahadir Tuluce <eminbahadir98@gmail.com>
2025-01-13 19:07:58 +0000

    Add failing test

---
+++ src/commands/generate/__tests__/resources/readonly-test-cases/example-types/product/emptyUnionTypeExample.ts
@@ -0,0 +1,13 @@
+export type IEmptyUnionTypeExample = any;
+
+export interface IEmptyUnionTypeExampleVisitor<T> {
+    readonly 'unknown': (obj: IEmptyUnionTypeExample) => T;
+}
+
+function visit<T>(obj: IEmptyUnionTypeExample, visitor: IEmptyUnionTypeExampleVisitor<T>): T {
+    return visitor.unknown(obj);
+}
+
+export const IEmptyUnionTypeExample = {
+    visit: visit
+};
